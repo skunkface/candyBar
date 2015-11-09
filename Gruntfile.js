696f13681717bc2c60ad3deabc6fc910a4cfcca6
skunkface <chris.miller4402@me.com>
2015-11-09 12:41:24 -0600

    added csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,23 @@ module.exports = function(grunt) {
           'candyBar.min.css': ['candyBar.css']
         }
       }
+    },
+    csslint: {
+      strict: {
+        options: {
+          import: 2
+        },
+        src: ['candyBar.css']
+      },
+      lax: {
+        options: {
+          import: false
+        },
+        src: ['candyBar.css']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
 };
